0204729abd51d4e47cebc57439ef67cf54ae59bb
naive2022 <70676258+naive2022@users.noreply.github.com>
2022-02-07 11:44:06 +0200

    Create helper-hardhat-config.js

---
+++ helper-hardhat-config.js
@@ -0,0 +1,23 @@
+const networkConfig = {
+ 
+
+    137: {
+        name: 'polygon',
+
+    },
+    4: {
+        name: 'rinkeby',
+        
+    },
+    80001: {
+        name: 'mumbi',
+        
+    }
+    
+}
+
+
+
+module.exports = {
+networkConfig,
+}
